b82cafb55bdfc5ef749ee5990b67bb701f37acb2
sphan <28304961+sanhphanvan96@users.noreply.github.com>
2023-12-08 18:35:41 +0700

    Improve nix package

--- pkg/nix/default.nix
+++ pkg/nix/default.nix
@@ -1,30 +1,34 @@
 # nix-build -E 'with import <nixpkgs> { }; callPackage ./default.nix { }'
 # nix-shell -E 'with import <nixpkgs> {}; callPackage ./default.nix { }'
+# nix-env -i ./result
 
-{ stdenv
-, lib
-, fetchFromGitHub
-, bash
-, subversion
-, makeWrapper
-}:
-  stdenv.mkDerivation {
-    pname = "lolcommit";
-    version = "v1.0.1";
-    src = fetchFromGitHub {
-      owner = "sanhphanvan96";
-      repo = "lolcommit";
-      rev = "v1.0.1";
-      sha256 = "sha256-5jAkLv7fpeNVHOW1cFwGCV573JkypxInwZs/zu4co+A=";
-    };
-    buildInputs = [ bash subversion ];
-    nativeBuildInputs = [ makeWrapper ];
-    installPhase = ''
-      mkdir -p $out/bin
-      mkdir -p $out/themes
-      cp bin/lolcommit.sh $out/bin/lolcommit.sh
-      cp themes/*.txt $out/themes
-      wrapProgram $out/bin/lolcommit.sh \
-        --prefix PATH : ${lib.makeBinPath [ bash subversion ]}
-    '';
-  }
+{ stdenv, lib, fetchFromGitHub, bash, subversion, makeWrapper }:
+let
+  pname  = "lolcommit";
+  gitUrl = "https://github.com/sanhphanvan96/lolcommit.git";
+  gitOwner = "sanhphanvan96";
+  gitRepo  = "lolcommit";
+  gitTag = "v1.0.1";
+  gitSHA256 = "sha256-CYmEsGLdJu6JdEdw4iFgA97/AKFvsFR7jixiw8q8r4g=";
+
+in stdenv.mkDerivation {
+  pname = "lolcommit";
+  version = gitTag;
+  src = fetchFromGitHub {
+    owner = gitOwner;
+    repo  = gitRepo;
+    rev   = gitTag;
+    sha256 = gitSHA256;
+  };
+
+  buildInputs = [ bash subversion ];
+  nativeBuildInputs = [ makeWrapper ];
+  installPhase = ''
+    mkdir -p $out/bin
+    mkdir -p $out/themes
+    cp bin/lolcommit.sh $out/bin/lolcommit
+    cp themes/*.txt $out/themes
+    wrapProgram $out/bin/lolcommit \
+      --prefix PATH : ${lib.makeBinPath [ bash subversion ]}
+  '';
+}
